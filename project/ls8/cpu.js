748eba0361d808a9bf342dab7ec8b151227a5111
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-06-14 16:02:42 -0700

    Subroutine Calls implemented - MVP requirements met

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -10,6 +10,11 @@ const HLT = 0b00000001;
 const MUL = 0b10101010;
 const PUSH = 0b01001101;
 const POP = 0b01001100;
+const CALL = 0b01001000;
+const RET = 0b00001001;
+const ADD = 0b10101000;
+const SUB = 0b10101001;
+const DIV = 0b10101011;
 
 const SP = 7;
 
@@ -26,10 +31,10 @@ class CPU {
 
         this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
         
-        this.reg[SP] = 0xF4; // Initialized stack pointer
-
         // Special-purpose registers
         this.PC = 0; // Program Counter
+        
+        this.reg[SP] = 0xF4; // Initialized stack pointer
     }
     
     /**
@@ -44,8 +49,7 @@ class CPU {
      */
     startClock() {
         this.clock = setInterval(() => {
-            this.tick();
-        }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
+            this.tick() }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
     }
 
     /**
@@ -72,6 +76,31 @@ class CPU {
                 this.reg[regA] *= this.reg[regB];
                 // this.reg[regA] = (this.reg[regB] * this.reg[regA]) & 0xff;
                 break;
+
+                case "DIV":
+                if (this.reg[regB] === 0) this.stopClock();
+                else this.reg[regA] /= this.reg[regB];
+                break;
+
+            case "ADD":
+                this.reg[regA] += this.reg[regB];
+                break;
+
+            case "SUB": 
+                this.reg[regA] -= this.reg[regB];
+                break;
+
+            case "INC":
+                this.reg[regA] += 1;
+                break;
+
+            case "DEC":
+                this.reg[regA] -= 1;
+                break;
+
+            default:
+                console.log("ERROR: something went wrong within the ALU switch/case.");
+                this.stopClock();
         }
     }
 
@@ -85,6 +114,7 @@ class CPU {
         // right now.)
         
         // !!! IMPLEMENT ME
+        // let increment = true;
         const IR = this.ram.read(this.PC);
 
         // Debugging output
@@ -99,6 +129,8 @@ class CPU {
 
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
+
+        let pcAdvance = true; //incrementing
 
         // !!! IMPLEMENT ME 
         switch(IR) {
@@ -124,13 +156,40 @@ class CPU {
                 break;
 
             case PUSH:
-                this.reg[SP]--;
+                this.reg[SP]--; //incrementing by moving stack in memory down by 1
                 this.ram.write(this.reg[SP], this.reg[operandA]);
                 break;
 
             case POP:    
                 this.reg[operandA] = this.ram.read(this.reg[SP]);
+                this.reg[SP]++; //incrementing by moving stack in memory up by 1
+                break;
+
+            case CALL: 
+                this.reg[SP]--; //should move down by one
+                // this.pushValue(this.reg[SP], this.PC + 2);
+                this.ram.write(this.reg[SP], this.PC + 2);
+                this.PC = this.reg[operandA];
+                //TODO set the PC to the address of the subroutine
+                pcAdvance = false; //incrementing
+                break;
+
+            case RET: 
+                this.PC = this.ram.read(this.reg[SP]);
                 this.reg[SP]++;
+                pcAdvance = false; //incrementing 
+                break;
+
+            case ADD: 
+                this.alu("ADD", operandA, operandB);
+                break;
+
+            case DIV: 
+                this.alu("DIV", operandA, operandB); 
+                break;
+
+            case SUB: 
+                this.alu("SUB", operandA, operandB); 
                 break;
 
             default:
@@ -143,11 +202,15 @@ class CPU {
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
         // instruction byte tells you how many bytes follow the instruction byte
         // for any particular instruction.
-        
+
         // !!! IMPLEMENT ME
-        const instLen = (IR >> 6) + 1;
-        this.PC += instLen;
+        if (pcAdvance) this.PC += (IR >> 6) + 1;
+        }
     }
-}
+
+        // pushValue(v) {
+        //     this.reg[SP]--;
+        //     this.ram.write(this.reg[SP], v);
+        // }
 
 module.exports = CPU;
